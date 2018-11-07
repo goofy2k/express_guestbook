3cf5e4c7b6ead77fc600738fa43b4d724a80de0e
Goofy2k <goofy2k@gmail.com>
2018-11-07 20:43:36 +0000

    Added more code for login logout authentication etc.  mysql access to be done.

--- guestbook/app.js
+++ guestbook/app.js
@@ -1,12 +1,14 @@
 var mysql =  require("mysql");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
+var passport = require("passport");
 var flash = require("connect-flash");
 //puts all of your routes in another file
-//var routes = require("./routes");
+var routes = require("./routes");
 
 var http = require("http");
 var path = require("path");
+var setUpPassport = require("./setuppassport");
 var express = require("express");
 //staticPath is the root for all static content
 //browser, views etc should refer to / for content in the folder public
@@ -20,6 +22,8 @@ app.use(express.static(staticPath));
 
 //connects to your mysql server and express_test
 // code here
+//mongoose.connect("mongodb://localhost:27017/test");
+setUpPassport();
 
 //new code....
 //app.set("port", process.env.PORT || 3000);
@@ -34,38 +38,44 @@ app.use(logger("dev"));
 //***** Uses four middlewares *****
 app.use(bodyParser.urlencoded({ extended: false }));
 //new code
-//app.use(cookieParser());
-//app.use(session({
-//secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
-//resave: true,
-//saveUninitialized: true
-//}));
-//app.use(flash());
-//app.use(routes);
+app.use(cookieParser());
+app.use(session({
+secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+resave: true,
+saveUninitialized: true
+}));
+app.use(flash());
+
+//for logging in
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(routes);
 
 //*** Some routes from the guestbook example *****
 //*** must be  moved to router.js ???? ******
-app.get("/guestbook", function(request, response) {
-response.render("index");
-});
-app.get("/new-entry", function(request, response) {
-response.render("new-entry");
-});
-app.post("/new-entry", function(request, response) {
-if (!request.body.title || !request.body.body) {
-response.status(400).send("Entries must have a title and a body.");
-return;
-}
-entries.push({
-title: request.body.title,
-content: request.body.body,
-published: new Date()
-});
-response.redirect("/guestbook");
-});
-app.use(function(request, response) {
-response.status(404).render("404");
-});
+//app.get("/guestbook", function(request, response) {
+//response.render("index");
+//});
+//app.get("/new-entry", function(request, response) {
+//response.render("new-entry");
+//});
+//app.post("/new-entry", function(request, response) {
+//if (!request.body.title || !request.body.body) {
+//response.status(400).send("Entries must have a title and a body.");
+//return;
+//}
+
+//entries.push({
+//title: request.body.title,
+//content: request.body.body,
+//published: new Date()
+//});
+//response.redirect("/guestbook");
+//});
+//app.use(function(request, response) {
+//response.status(404).render("404");
+//});
 //*** END OF GUESTBOOK ROUTES ***
 
 http.createServer(app).listen(3000, function() {
--- guestbook/routes.js
+++ guestbook/routes.js
@@ -3,19 +3,142 @@
 
 var express = require("express");
 var User = require("./models/user");
+var passport = require("passport");
+
 var router = express.Router();
+
 router.use(function(req, res, next) {
-res.locals.currentUser = req.user;
-res.locals.errors = req.flash("error");
-res.locals.infos = req.flash("info");
-next();
-});
+	res.locals.currentUser = req.user;
+	res.locals.errors = req.flash("error");
+	res.locals.infos = req.flash("info");
+	next();
+	});
 router.get("/", function(req, res, next) {
-User.find()
-.sort({ createdAt: "descending" })
-.exec(function(err, users) {
+//	User.find()
+//	.sort({ createdAt: "descending" })
+//	.exec(function(err, users) {
+//	if (err) { return next(err); }
+
+//		res.render("_index", { users: users });
+//		});
+// render page with dummy result of the query
+res.render("_index", {} );
+	});
+
+
+router.get("/signup", function(req, res) {
+res.render("signup");
+});
+router.post("/signup", function(req, res, next) {
+var username = req.body.username;
+var password = req.body.password;
+User.findOne({ username: username }, function(err, user) {
 if (err) { return next(err); }
-res.render("_index", { users: users });
+if (user) {
+req.flash("error", "User already exists");
+return res.redirect("/signup");
+}
+var newUser = new User({
+username: username,
+password: password
+});
+newUser.save(next);
+});
+}, passport.authenticate("login", {
+successRedirect: "/",
+failureRedirect: "/signup",
+failureFlash: true
+}));
+
+router.post("/login", passport.authenticate("login", {
+successRedirect: "/",
+failureRedirect: "/login",
+failureFlash: true
+}));
+router.get("/users/:username", function(req, res, next) {
+User.findOne({ username: req.params.username }, function(err, user) {
+if (err) { return next(err); }
+if (!user) { return next(404); }
+res.render("profile", { user: user });
 });
 });
+
+
+router.get("/login", function(req, res) {
+res.render("login");
+});
+
+router.get("/logout", function(req, res) {
+req.logout();
+res.redirect("/");
+});
+
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) {
+next();
+} else {
+req.flash("info", "You must be logged in to see this page.");
+res.redirect("/login");
+}
+}
+
+
+router.get("/edit", ensureAuthenticated, function(req, res) {
+res.render("edit");
+});
+
+router.post("/edit", ensureAuthenticated, function(req, res, next) {
+req.user.displayName = req.body.displayname;
+req.user.bio = req.body.bio;
+req.user.save(function(err) {
+if (err) {
+next(err);
+return;
+}
+req.flash("info", "Profile updated!");
+res.redirect("/edit");
+});
+});
+
+
+//*** Some routes from the guestbook example *****
+//*** must be  moved to router.js ???? ******
+
+router.get("/guestbook", function(request, response) {
+response.render("index");
+});
+
+router.get("/new-entry", function(request, response) {
+response.render("new-entry");
+});
+router.post("/new-entry", function(request, response) {
+if (!request.body.title || !request.body.body) {
+response.status(400).send("Entries must have a title and a body.");
+return;
+}
+
+entries.push({
+                title: request.body.title,
+                content: request.body.body,
+                published: new Date()
+        });
+response.redirect("/guestbook");
+});
+
+router.use(function(request, response) {
+response.status(404).render("404");
+});
+//*** END OF GUESTBOOK ROUTES ***
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+//rendering needs a  user with : name, username, bio
+
